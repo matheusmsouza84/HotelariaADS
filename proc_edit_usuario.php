7a62a2b153dd77762324e136290bf658acdf1fab
GUSTAVO PEREIRA GASPAR <52473811+GGaspar30@users.noreply.github.com>
2020-11-12 21:15:43 -0300

    Subindo proc_edit_usuario

---
+++ proc_edit_usuario.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include_once "model/clsConexao.php";
+
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+echo "Nome: $nome <br>";
+echo "E-mail: $email <br>";
+
+
+$query = "UPDATE usuarios SET nome='$nome', email='$email', modified=NOW() WHERE id='$id'";
+$result = Conexao::consultar($query);
+
+if(mysqli_affected_rows($conn)){
+	$_SESSION['msg'] = "<p style='color:green;'>Usuário editado com sucesso</p>";
+	header("Location: edit_usuario.php");
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi editado com sucesso</p>";
+	header("Location: edit_usuario.php?id=$id");
+}
+?>
