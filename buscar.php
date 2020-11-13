2d9cb09ea10435846544f823e587c5bbc96f7eb9
GUSTAVO PEREIRA GASPAR <52473811+GGaspar30@users.noreply.github.com>
2020-11-12 21:12:55 -0300

    Update buscar.php
    Subindo pesquisar

--- buscar.php
+++ buscar.php
@@ -1,40 +1,68 @@
+<?php
+include_once "model/clsConexao.php";
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <title> Cadastro </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Página Inicial</title>
 </head>
     <body>
-        <div class="header">Hotel</div>
-            <nav class="navbar navbar-expand-md">
-                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a href="CadastrarCliente.php">
-                                <button class="btn btn-primary" type="button">Página Inicial</button>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <div class="content">
-                <div class="shadow-lg bg-white" id="divsombra">
-                    <h4><b>Buscar Cadastro</b></h4><br>
-                    <form method="POST" id="formbusca" action="XXXXXX" >
-                        <input type="text" placeholder="Insira seu CPF" name="cpfb" required> <br> <br>
-                        <input class="btn btn-success" type="submit" value="Buscar">
-                        </form>
-                </div>
-                <div class="show">
-
-                </div>
-            </div><br><br>
-            <div class="show">
-
+    <div class="header">Hotel</div>
+        <nav class="navbar navbar-expand-md">
+            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                    <a href="index.php">
+                        <button type="button" id="paginicial" title="Voltar a página Inicial">
+                        <svg class="bi bi-house-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                        </svg>
+                        </button>
+                        </a>
+                    </li>
+                </ul>
             </div>
-                <div class="footer"></div>
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                     </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="content"><br><br><br><br><br><br><br>
+                <div class="shadow-lg bg-white" id="divcadastro">
+		
+		<h1>Pesquisar Usuário</h1>
+		
+		<form method="POST" action="">
+			<label>CPF: </label>
+			<input type="text" name="cpf" placeholder="Digite o cpf" required><br><br>
+			
+			<input name="SendPesqUser" type="submit" value="Pesquisar">
+		</form><br><br>
+		
+		<?php
+		$SendPesqUser = filter_input(INPUT_POST, 'SendPesqUser', FILTER_SANITIZE_STRING);
+		if($SendPesqUser){
+			$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
+			$query = "SELECT * FROM clientes WHERE cpf LIKE '%$cpf%'";
+			$result = Conexao::consultar($query);
+			while($row_usuario = mysqli_fetch_assoc($result)){	
+				echo "Nome: " . $row_usuario['nome'] . "<br>";
+				echo "<a href='edit_usuario.php?id=" . $row_usuario['id'] . "'>Editar</a><br>";
+				
+			}
+		}
+		?>
+                </div>
+        </div>
+    <div class="footer"></div>
     </body>
 </html>
