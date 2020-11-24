f97933d93856aea451f934b729ee92037ae203f8
Matheus Melchior Souza <61211870+matheusmsouza84@users.noreply.github.com>
2020-11-24 19:31:54 -0300

    Add files via upload

--- export.php
+++ export.php
@@ -2,6 +2,7 @@
 session_start();
 include_once "model/clsConexao.php";
 
+
     $query = "SELECT nome, rg, cpf, cep FROM clientes WHERE statusc = 1";
     $result = Conexao::consultar($query);
     
@@ -11,9 +12,12 @@ include_once "model/clsConexao.php";
     }
     
     file_put_contents('dados.csv', $str);
-   
+    setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' ); 
+    date_default_timezone_set( 'America/Sao_Paulo' );
+    $data =  strftime( '%d-%m-%Y %H:%M:%S', strtotime('now') ); 
+    
     header("Content-type: text/csv");
-    header("Content-Disposition: attachment; filename=dados.csv");
+    header("Content-Disposition: attachment; filename=dados-".$data.".csv");
     header("Pragma: no-cache");
     header("Expires: 0");
     
@@ -21,4 +25,3 @@ echo $str;
 
 
 ?>
-
