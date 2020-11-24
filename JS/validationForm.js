8820388aba0dfa5983b12aac8e6a359a06eda8d1
Matheus Melchior Souza <61211870+matheusmsouza84@users.noreply.github.com>
2020-11-24 19:47:19 -0300

    Add files via upload

--- JS/validationForm.js
+++ JS/validationForm.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-$("#cpf").mask("000.000.000-00");
+//$("#cpf").mask("000.000.000-00");
 $("#telefone").mask("(00)00000-0000");
 $("#datanasc").mask("00/00/0000");
 $("#rg").mask("0000000000");
