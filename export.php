d4cfff5cd5942ca78a711317aa450afea0723d24
Matheus Melchior Souza <61211870+matheusmsouza84@users.noreply.github.com>
2020-11-26 20:30:55 -0300

    nome do arquivo com ponto

--- export.php
+++ export.php
@@ -7,15 +7,6 @@ include_once "model/clsConexao.php";
     $result = Conexao::consultar($query);
      
     $str = "";
-    $str = '<table>';
-    $str = '<tr>';
-    $str = '<td><b>Coluna 1</b></td>';
-    $str = '<td><b>Coluna 2</b></td>';
-    $str = '<td><b>Coluna 3</b></td>';
-    $str = '<td><b>Coluna 4</b></td>';
-    $str = '</tr>'; 
-    $str = '</table>'; 
-    $str = "";
     while ($row_cliente = mysqli_fetch_assoc($result)){
         $str .= implode(';', $row_cliente) .';'. PHP_EOL;
     }
@@ -23,7 +14,7 @@ include_once "model/clsConexao.php";
     file_put_contents('dados.csv', $str);
     setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' ); 
     date_default_timezone_set( 'America/Sao_Paulo' );
-    $data =  strftime( '%d-%m-%Y(%H;%M;%S)', strtotime('now') ); 
+    $data =  strftime( '%d-%m-%Y(%H.%M.%S)', strtotime('now') ); 
     
     header("Content-type: text/csv");
     header("Content-Disposition: attachment; filename=dados-".$data.".csv");
